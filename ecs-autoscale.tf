930468cacc39bee3e0d2e5f2bd5461eea606892c
Lucas Ewalt <lucas.ewalt@gmail.com>
2018-11-19 12:17:05 -0500

    initial terraform config to provision 1 node AWS ECS cluster. Includes required service and instance IAM roles to interact with ECS services and also basic autoscale config to spin up 1 ECS cluster instance in 1 availability zone. Must specify secrets variable file and run with terraform apply -var-file="secrets.tfvars"

---
+++ ecs-autoscale.tf
@@ -0,0 +1,38 @@
+data "aws_ami" "ecs" {
+  most_recent = true
+
+  filter {
+    name    = "name"
+    values  = ["*amzn2-ami-ecs-hvm*"]
+  }
+}
+
+resource "aws_launch_configuration" "ecs-launch-config" {
+  name                  = "ecs_launch_config"
+  image_id              = "${data.aws_ami.ecs.id}"
+  instance_type         = "t2.micro"
+  iam_instance_profile  = "${aws_iam_instance_profile.ecs-instance-profile.id}"
+  key_name              = "linux-ssh-key"
+
+  # needed for autoscale created ec2 instance to join ecs cluster
+  user_data             = <<EOF
+#!/bin/bash
+echo ECS_CLUSTER=container-cluster >> /etc/ecs/ecs.config
+                          EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "ecs-autoscale" {
+  name                  = "ecs_autoscale_group"
+  launch_configuration  = "${aws_launch_configuration.ecs-launch-config.name}"
+  availability_zones    = ["us-east-2a"]
+  min_size              = 1
+  max_size              = 20
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
