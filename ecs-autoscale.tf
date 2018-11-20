288c84fd96b2c2b1c815e1c24ed4cdaa4d58ba4a
Lucas Ewalt <lucas.ewalt@gmail.com>
2018-11-20 09:57:30 -0500

    add auto-scale policy for the ECS service, set to add/remove cluster nodes as needed to maintain a target CPU utilization across cluster

--- ecs-autoscale.tf
+++ ecs-autoscale.tf
@@ -1,3 +1,4 @@
+# get ami ID for AWS ECS Linux 2
 data "aws_ami" "ecs" {
   most_recent = true
 
@@ -26,12 +27,28 @@ echo ECS_CLUSTER=container-cluster >> /etc/ecs/ecs.config
   }
 }
 
+# use target tracking scaling to aim to keep average CPU util at a given target
+# https://docs.aws.amazon.com/autoscaling/ec2/userguide/as-scaling-target-tracking.html
+resource "aws_autoscaling_policy" "ecs-autoscale" {
+  name  = "ecs-scale-up"
+  policy_type = "TargetTrackingScaling"
+  adjustment_type = "ChangeInCapacity"
+  autoscaling_group_name = "${aws_autoscaling_group.ecs-autoscale.name}"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 60.0
+  }
+}
+
 resource "aws_autoscaling_group" "ecs-autoscale" {
   name                  = "ecs_autoscale_group"
   launch_configuration  = "${aws_launch_configuration.ecs-launch-config.name}"
   availability_zones    = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  min_size              = 3
-  max_size              = 20
+  min_size              = 1
+  max_size              = 5
 
   lifecycle {
     create_before_destroy = true
